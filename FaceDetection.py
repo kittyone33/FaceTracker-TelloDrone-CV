c81d88e6c018572d7dbc14408fa13f37d52efdd4
kittyone33 <69946855+kittyone33@users.noreply.github.com>
2022-11-06 08:37:14 +0000

    fix bg

--- FaceDetection.py
+++ FaceDetection.py
@@ -7,4 +7,10 @@ TOLERANCE_Y = 5
 SLOWDOWN_THRESHOLD_X = 20
 SLOWDOWN_THRESHOLD_Y = 20
 DRONE_SPEED_X = 20
-DRONE_SPE
+DRONE_SPEED_Y = 20
+SET_POINT_X = 960/2
+SET_POINT_Y = 720/2
+
+
+cascPath = sys.argv[1]  # Path of the model used to reveal faces
+faceCascade = cv2.CascadeClassifier(cascPath
